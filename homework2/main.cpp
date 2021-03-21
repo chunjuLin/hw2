8ed376e6c4e5a80be64178fbe08d15c95ca71442
chunjuLin <linchun0308@gmail.com>
2021-03-21 04:21:20 -0700

    add main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -7,7 +7,10 @@ DigitalIn down(D3);
 DigitalIn up(D4);
 DigitalIn selected(D5);
 
-int frequency = 100; // in Hz
+AnalogOut aout(PA_4);
+//AnalogIn filted_signal(A1);
+
+float frequency = 400; // in Hz
 int pre_down;
 int pre_up;
 int done = 0;
@@ -19,36 +22,51 @@ int main()
     uLCD.line(29, 73, 99, 73, WHITE);
     uLCD.line(29, 55, 29, 73, WHITE);
     uLCD.line(99, 55, 99, 73, WHITE);
-
-
    // press the button and see the console / led change
-   while (1)
+   while (done == 0)
    {
         if(selected.read() == 1) done = 1;
         if(done == 0){
             if(pre_down != down.read() && down.read() == 1){
-                if(frequency > 100){
+                if(frequency >= 400){
                     frequency -= 100;
                 }
             } 
             else if(pre_up != up.read() && up.read() == 1){
-                if(frequency < 700){
+                if(frequency < 1000){
                     frequency += 100;
                 }
             }
             pre_down = down.read();
             pre_up = up.read();
         }
-        uLCD.locate(4,5); uLCD.printf("%d Hz",frequency);
-        uLCD.filled_rectangle(30, 56, 30 + frequency/10 - 1, 72, 0x00FF00);
-        if(frequency != 700)uLCD.filled_rectangle(30 + frequency/10 , 56, 98, 72, BLACK);
+        uLCD.locate(4,5); uLCD.printf("%d  Hz",(int)frequency);
+        uLCD.filled_rectangle(30, 56, 30 + (frequency - 300)/10 - 1, 72, 0x00FF00);
+        if(frequency != 1000)uLCD.filled_rectangle(30 + (frequency - 300)/10 , 56, 98, 72, BLACK);
         uLCD.line(99, 55, 99, 73, WHITE);
        // uLCD.cls();
        //uLCD.reset();
        // uLCD.line(29, 55, 29 + frequency/10, 73, WHITE);
      // printf("value down : %d \n\r", down.read());
-      printf("select : %d \n\r", selected.read());
-      printf("frequency: %d \n\r", frequency);
+     // printf("select : %d \n\r", selected.read());
+      printf("frequency: %f \n\r", frequency);
       //ThisThread::sleep_for(250ms);
+   }
+   while (done == 1){
+        float T = 1 / frequency;
+        float dt = T / 10 * 1000000;
+        //printf("T: %f\n\r",T);
+        //printf("dt: %f\n\r",dt);
+        //printf("dt_int: %d\n\r",(int)dt);
+        for (float i = 0.0f; i <= 0.9f; i += 0.1125f) {
+            aout = i;
+            //printf("aout = %f volts\n", aout.read() * 3.3f);
+            wait_us((int)dt);
+        }
+        for (float i = 0.9f; i >= 0.0f; i -= 0.45f) {
+            aout = i;
+            //printf("aout = %f volts\n", aout.read() * 3.3f);
+            wait_us((int)dt);
+        }
    }
 }
