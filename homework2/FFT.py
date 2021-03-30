9d12bf611e8aa648e17d733afa6b7061700219de
chunjuLin <linchun0308@gmail.com>
2021-03-30 09:19:37 -0700

    add FFT.py

--- homework2/FFT.py
+++ homework2/FFT.py
@@ -3,10 +3,13 @@ import numpy as np
 import serial
 import time
 
-Fs = 1000.0;  # sampling rate
-Ts = 1.0/Fs; # sampling interval
-t = np.arange(0,1,Ts) # time vector; create Fs samples between 0 and 1.0 sec.
-y = np.arange(0,1,Ts) # signal vector; create Fs samples
+serdev = '/dev/ttyACM0'
+
+s = serial.Serial(serdev)
+Fs = int(s.readline())  # sampling rate
+Ts = 1.0/Fs # sampling interval
+t = np.arange(0,100/Fs*10,Ts) # time vector; create Fs samples between 0 and 1.0 sec.
+y = np.arange(0,100/Fs*10,Ts) # signal vector; create Fs samples
 
 n = len(y) # length of the signal
 k = np.arange(n)
@@ -14,9 +17,7 @@ T = n/Fs
 frq = k/T # a vector of frequencies; two sides frequency range
 frq = frq[range(int(n/2))] # one side frequency range
 
-serdev = '/dev/ttyACM0'
-s = serial.Serial(serdev)
-for x in range(0, int(Fs)):
+for x in range(0, 1000):
     line=s.readline() # Read an echo string from B_L4S5I_IOT01A terminated with '\n'
     # print line
     y[x] = float(line)
